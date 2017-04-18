aa597cf6dc32c42b1a2511645bc346883b6a4f84
eron <eronryabets@gmail.com>
2017-04-19 00:58:27 +0300

    upload

---
+++ TestCode.py
@@ -0,0 +1,42 @@
+# THE MYSTICAL OCTOSPHERE!
+
+
+import random
+import time
+
+# message = ""
+print("Задайте свой вопрос магическому Шару...")
+time.sleep(3)
+
+def number_to_fortune(num):
+    if num == 0:
+        return "Да, точно!"
+
+    elif num == 1:
+        return "Вероятно, да ..."
+
+    elif num == 2:
+        return "Кажется, да ..."
+
+    elif num == 3:
+        return "Определенно нет!"
+
+    elif num == 4:
+        return "Вероятно, нет"
+
+    elif num == 5:
+        return "Я действительно в этом сомневаюсь ..."
+
+    elif num == 6:
+        return "Не уверен, зайдите позже!"
+
+    elif num == 7:
+        return "Я действительно не могу сказать"
+
+    else:
+        return "Я обычный шар, а не предсказатель!"
+
+num = random.randrange(0, 7)
+print(number_to_fortune(num))
+#TEST#print(number_to_fortune(17))
+#time.sleep(3) # zaderzka ispolnenia v 3 sec
