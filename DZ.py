25c599dd560245f96688ee7136912f3cb18a992e
eron <eronryabets@gmail.com>
2017-04-18 23:50:16 +0300

    upload

--- DZ.py
+++ DZ.py
@@ -63,3 +63,12 @@ print("Факториал вашего числа: ",fac(n))
 
 
 """
+"""
+n = int(input("Факториал числа ") )
+fac = 1
+i = 0
+while i < n:
+     i += 1
+     fac = fac * i
+print ("равен",fac)
+"""
